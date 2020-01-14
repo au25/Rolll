16223420302685b31daec7beabf8762142db5bcc
Oscar <au.oscar@hotmail.com>
2020-01-14 10:51:51 -0800

    signup buttons

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function(){
-    return (
-        <div>
-            This is the signup page.
-        </div>
-    )
+const useStyles = makeStyles({
+    signupPageContainer: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    fbButton: {
+        width: "300px"
+    },
+    googleButton: {
+        width: "300px"
+    },
+    accountButton: {
+        width: "300px"
+    }
+});
+
+export default function() {
+  const classes = useStyles();
+  return (
+    <div className={classes.signupPageContainer}>
+      <div className={classes.signupMsgContainer}>Sign up for an account</div>
+      <button className={classes.fbButton}>Sign up with Facebook</button>
+      <button className={classes.googleButton}>Sign up with Google</button>
+      <br />
+      <button className={classes.accountButton}>Sign up for an account</button>
+    </div>
+  );
 }
