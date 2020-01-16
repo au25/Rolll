5863bdab0a4b9ce34f6302823a6b73826e14355e
Oscar <au.oscar@hotmail.com>
2020-01-15 22:12:00 -0800

    sanitize

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY.replace(/\\n/g, '\n'),
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
