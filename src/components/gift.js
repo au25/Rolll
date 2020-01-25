3f32550fb912c793f1fdffebc785c35a23e5a012
Oscar <au.oscar@hotmail.com>
2020-01-25 14:05:16 -0800

    push to array updated

--- src/components/gift.js
+++ src/components/gift.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import app from "../firebase";
 
 const useStyles = makeStyles({
   giftContainer: {
@@ -25,13 +26,41 @@ const useStyles = makeStyles({
 
 export default function() {
   const classes = useStyles();
+  const [gift, setGift] = useState([]);
+  const db = app.firestore();
+
+  useEffect(() => {
+    async function fetchData() {
+      const snapshot = await db.collection("chat").get();
+      const docs = snapshot.docs;
+      for (let i = 0; i < 2; i++) {
+        setGift( gift => [
+          ...gift,
+          {
+            id: docs[i].id,
+            message: docs[i].data().message
+          }
+        ]);
+        console.log(docs[i].id);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const renderGift = () => {
+    let giftArray = [];
+    for (let i = 0; i < gift.length; i++) {}
+  };
 
   return (
     <div className={classes.giftContainer}>
+      {gift.map(data => (
+        <div> {data.message} </div>
+      ))}
       <div className={classes.giftImgShopName}>
         <div className={classes.giftImg}></div>
         <div className={classes.giftDescription}>
-          <div>Shop Name</div>
+          <div>Shp Name</div>
         </div>
       </div>
       <div>
