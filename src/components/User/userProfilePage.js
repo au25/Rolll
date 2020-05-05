5d88f942d9c1e4bf5f411366ecab8e2aa6cf99b7
Oscar <au.oscar@hotmail.com>
2020-05-05 07:01:19 -0700

    mobile userprofile css

--- src/components/User/userProfilePage.js
+++ src/components/User/userProfilePage.js
@@ -39,12 +39,38 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   userProfile_cityInputContainer: {
-    width: "65%",
+    width: "70%",
+    display: "flex",
+    borderBottom: "1px solid black",
   },
-  userProfile_cityTitle: {
-    width: "65%",
+  userProfile_emailInputContainer: {
+    width: "70%",
+    display: "flex",
+    borderBottom: "1px solid black",
+  },
+  userProfile_emailTitle: {
+    width: "70%",
     display: "flex",
     fontSize: "14px",
+    margin: "45px 0 0 0"
+  },
+  userProfile_cityTitle: {
+    width: "70%",
+    display: "flex",
+    fontSize: "14px",
+    margin: "45px 0 0 0"
+  },
+  userProfile_emailInput: {
+    [theme.breakpoints.down(600)]: {
+      width: "193px",
+      margin: "5px 0",
+      border: "none",
+      fontSize: "20px",
+      backgroundColor: "transparent",
+      padding: "0 0 1px 4px",
+      outline: "none",
+      fontSize: "16px",
+    },
   },
   userProfile_cityInput: {
     [theme.breakpoints.down(600)]: {
@@ -53,8 +79,7 @@ const useStyles = makeStyles((theme) => ({
       border: "none",
       fontSize: "20px",
       backgroundColor: "transparent",
-      borderBottom: "1px solid black",
-      padding: "0 0 5px 7px",
+      padding: "0 0 1px 4px",
       outline: "none",
       fontSize: "16px",
     },
@@ -62,17 +87,18 @@ const useStyles = makeStyles((theme) => ({
   userProfile_countryRegionContainer: {
     display: "flex",
     justifyContent: "space-between",
-    width: "65%",
+    width: "70%",
   },
   userProfile_countryRegionTitleContainer: {
     display: "flex",
+    width: "70%",
+    margin: "15px 0 0 0",
     justifyContent: "space-between",
-    width: "65%",
-    margin: "15px 0 0 0"
   },
   userProfile_countryDropdownContainer: {
     borderBottom: "1px solid black",
-    height: "100%"
+    height: "100%",
+    width: "120px",
   },
   userProfile_saveLogoutContainer: {
     display: "flex",
@@ -137,12 +163,14 @@ export default function ({ userDbInfo }) {
         className={classes.userProfile_formContainer}
         onSubmit={handleProfileUpdate}
       >
-        <TextField
-          disabled
-          label="Email"
-          value={userInfo.user_email}
-          className={classes.userProfile_input}
-        />
+        <div className={classes.userProfile_emailTitle}>Email</div>
+        <div className={classes.userProfile_emailInputContainer}>
+          <input
+            disabled
+            value={userInfo.user_email}
+            className={classes.userProfile_emailInput}
+          />
+        </div>
         <div className={classes.userProfile_cityTitle}>City</div>
         <div className={classes.userProfile_cityInputContainer}>
           <input
@@ -179,6 +207,7 @@ export default function ({ userDbInfo }) {
               value={userInfo.user_region}
               valueType="short"
               labelType="short"
+              disableWhenEmpty={true}
               onChange={(region) =>
                 setUserInfo({ ...userInfo, user_region: region })
               }
