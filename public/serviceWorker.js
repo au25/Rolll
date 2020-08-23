69648699cf13949da482cfcf6dd7f255749b468d
Oscar <au.oscar@hotmail.com>
2020-08-23 13:41:19 -0700

    optimizing lighthouse report

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,14 +1,20 @@
-const cache_ver = "ver1";
+const cache_ver = "ver1.4";
 const cacheArray = [
   "index.html",
   "offline.html",
-  "img/landing_withLogo.jpg"
+  "img/landing_withLogo.jpg",
+  "https://firebasestorage.googleapis.com/v0/b/owospace-d6985.appspot.com/o/images%2Fred_gift_box.png?alt=media&token=ace05975-618b-4e8d-b9fd-a7d83736e5eb",
+  "https://firebasestorage.googleapis.com/v0/b/owospace-d6985.appspot.com/o/images%2Fbackground_35.png?alt=media&token=a5be174a-cd06-4d97-b282-be6b33e71946",
+  "https://firebasestorage.googleapis.com/v0/b/owospace-d6985.appspot.com/o/images%2Fblue_gift_box2.png?alt=media&token=a1e7a0ab-bf4e-4c07-8ca2-d4a841aa06b9",
+
 ];
 
 // serviceWorker listens to install request
 // install only happens whenever the service worker is updated / installed for the first time
 self.addEventListener("install", (event) => {
   // waitUntil accepts a promise as a argument
+  // cache.open function takes longer than the install event, waitUntil funciton will
+  // allow cache event to finish before install event is finish
   event.waitUntil(
     (async () => {
       try {
@@ -45,9 +51,15 @@ self.addEventListener("activate", (event) => {
 // listens for fetch requests
 // fetch requests refers to any request to the server ( .html, .js, .css, libraries, images, etc)
 self.addEventListener("fetch", (event) => {
+  //   console.log("fetch event", event);
+  // pauses fetch event and respond with custom event
   event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
+    caches.match(event.request).then((cacheRes) => {
+      // return cache response or proceed with fetch
+      return (
+        cacheRes ||
+        fetch(event.request).catch(() => caches.match("offline.html"))
+      );
     })
   );
 });
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,26 @@
 import * as firebase from "firebase/app";
-import 'firebase/firestore';
+import "firebase/firestore";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY.replace(/\\n/g, '\n'),
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY.replace(/\\n/g, "\n"),
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
+
+app
+  .firestore()
+  .enablePersistence()
+  .catch(function (err) {
+    if (err.code == "failed-precondition") {
+      console.log("persistence error", err)
+    } else if (err.code == "unimplemented") {
+      console.log("persistence unimplemented", err)
+    }
+  });
 
 export default app;
